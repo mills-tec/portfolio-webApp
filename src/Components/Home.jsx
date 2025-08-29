bc84bcf38ac3a6a2ec19d45302b6f49ebfc3f80e
Talnetd Prince <princensude6@gmail.com>
2025-08-29 20:13:08 +0100

    corrected hreo image file

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImg from "../assets/HeroImg.png";
+import HeroImg from "../assets/heroImg.png";
 const Home = ({ serviceRef, motion }) => {
   // const ball = {
   //   width: "100%",
